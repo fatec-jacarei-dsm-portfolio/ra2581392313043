eaf2c5e04a3987dc924a188e328635d038f6faea
abnercosta97 <abner-rodrigo@hotmail.com>
2025-05-20 16:56:39 -0300

    ajustando base para /docs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ra2581392313043/", // Substitua pelo nome exato do repositório
+  base: "/ra2581392313043/docs", // Substitua pelo nome exato do repositório
 });
