c08551431a5e79276681d2f264bfa2d262b5690c
abnercosta97 <abner-rodrigo@hotmail.com>
2025-05-21 08:33:41 -0300

    deploy manual

--- docs/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/ra2581392313043/docs", // Substitua pelo nome exato do repositório
-});
